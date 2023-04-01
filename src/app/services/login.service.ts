f4c7eaa9e6e41eba8988fd5fd735b623680ff4c1
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-04-01 16:38:51 +0300

    Login service
    end point for login via username and password and receiving the user data.

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private readonly logEndPoint = "http://localhost:8080/student_helper/api/student";
+
+  constructor(private http: HttpClient) { }
+
+  getUser(username: string, password: string){
+    const headers = new HttpHeaders()
+    .set('Content-Type', 'application/json');
+
+    const data = {"username": username, "password": password};
+    return this.http.post(this.logEndPoint, data);
+  }
+}
