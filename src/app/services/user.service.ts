1c9b5b7aa9acbc442780b6322af51d7bd96f3085
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-04-01 16:39:08 +0300

    user service
    service to store user data

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  user!: User;
+
+  setUser(data: any){
+    this.user = data.data.student;
+    console.log(data);
+    console.log(data.data);
+  }
+
+  getUser(){
+    return this.user;
+  }
+}
+
+export interface User{
+  info: {
+    am: string,
+    firstName: string,
+    lastName: string,
+    department: string,
+    currentSemester: number,
+    registrationYear: number,
+    curriculum: string
+  },
+  grades: {
+    totalPassedCourses: number,
+    totalAverageGrade: number,
+    totalEcts: number,
+    courses: Course[]
+  }
+}
+
+export interface Course{
+  id: string,
+  name: string,
+  grade: number
+}
