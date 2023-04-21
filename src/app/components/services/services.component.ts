5039e251ac067cd9ea5ca35b1d982935994dbeb4
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-04-21 12:58:52 +0300

    removed commented code

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -18,21 +18,6 @@ export class ServicesComponent {
     this.user = this.userService.getUser();
     this.registrationYear = this.user.info.registrationYear;
     this.curriculum = this.user.info.curriculum;
-    // this.deletionYear = this.calculateYearOfDeletion();
     this.deletionYear = this.user.info.deletionYear;
   }
-
-  // calculateYearOfDeletion():number {
-  //   if (this.curriculum=='ΠΡΟΓΡΑΜΜΑ 5 ΕΤΩΝ ΣΠΟΥΔΩΝ (2019)'){
-  //     if (this.registrationYear<2015) return 2026;
-  //     if (this.registrationYear<2021) return 2029;
-  //     if (this.registrationYear>=2022) return this.registrationYear+8;
-  //   }
-  //   else {
-  //     if (this.registrationYear<=2016) return 2025;
-  //     if (this.registrationYear<2021) return 2027;
-  //     if (this.registrationYear>=2022) return this.registrationYear+6;
-  //   }
-  //   return -1;
-  // }
 }
