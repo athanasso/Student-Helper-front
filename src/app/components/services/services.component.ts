4d763cffe4e4e0f8ed0c5f019eea612f12e4f15e
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-04-24 15:42:52 +0300

    moved deletion year

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -7,17 +7,5 @@ import { User, UserService } from 'src/app/services/user.service';
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent {
-  constructor(private userService: UserService){}
 
-  user!: User;
-  registrationYear!: number;
-  curriculum!: string;
-  deletionYear!: string;
-
-  ngOnInit(): void {
-    this.user = this.userService.getUser();
-    this.registrationYear = this.user.info.registrationYear;
-    this.curriculum = this.user.info.curriculum;
-    this.deletionYear = this.user.info.deletionYear;
-  }
 }
