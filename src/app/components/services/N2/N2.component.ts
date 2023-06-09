53594a758606f9b379d54b793cfa2f87bbdd969d
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-06-09 14:21:15 +0300

    N2 curriculum

--- src/app/components/services/N2/N2.component.ts
+++ src/app/components/services/N2/N2.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-N2',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./N2.component.scss']
 })
 export class N2Component {
+  neededCourses: any;
+  @ViewChild('mandatoryPaginator') mandatoryPaginator!: MatPaginator;
+  @ViewChild('basicPaginator') basicPaginator!: MatPaginator;
+  @ViewChild('choiceFromSameBasic1Paginator') choiceFromSameBasic1Paginator!: MatPaginator;
+  @ViewChild('choiceFromSameBasic2Paginator') choiceFromSameBasic2Paginator!: MatPaginator;
+  @ViewChild('choiceFromOtherBasicPaginator') choiceFromOtherBasicPaginator!: MatPaginator;
 
+  mandatoryCourses!: MatTableDataSource<any>;
+  basicCourses!: MatTableDataSource<any>;
+  choiceCoursesFromSameBasic1!: MatTableDataSource<any>;
+  choiceCoursesFromSameBasic2!: MatTableDataSource<any>;
+  choiceCoursesFromOtherBasic!: MatTableDataSource<any>;
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    let user = this.userService.getUser();
+    this.neededCourses = user.grades.neededCourses;
+    this.mandatoryCourses = new MatTableDataSource(this.neededCourses.mandatoryCoursesLeft);
+    this.basicCourses = new MatTableDataSource(this.neededCourses.basicCoursesLeft);
+    this.choiceCoursesFromSameBasic1 = new MatTableDataSource(this.neededCourses.choice1CoursesLeft);
+    this.choiceCoursesFromSameBasic2 = new MatTableDataSource(this.neededCourses.choice2CoursesLeft);
+    this.choiceCoursesFromOtherBasic = new MatTableDataSource(this.neededCourses.choiceFromOthersBasicAvailable);
+  }
+
+  ngAfterViewInit(){
+    this.mandatoryCourses.paginator = this.mandatoryPaginator;
+    this.basicCourses.paginator = this.basicPaginator;
+    this.choiceCoursesFromSameBasic1.paginator = this.choiceFromSameBasic1Paginator;
+    this.choiceCoursesFromSameBasic2.paginator = this.choiceFromSameBasic2Paginator;
+    this.choiceCoursesFromOtherBasic.paginator = this.choiceFromOtherBasicPaginator;
+  }
 }
