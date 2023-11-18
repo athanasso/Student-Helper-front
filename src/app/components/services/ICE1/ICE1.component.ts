14e50ed64474483fff78e39a7ad78440eff019fe
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-11-19 00:50:51 +0200

    adjusted ui for new selection of flow

--- src/app/components/services/ICE1/ICE1.component.ts
+++ src/app/components/services/ICE1/ICE1.component.ts
@@ -11,15 +11,27 @@ import { UserService } from 'src/app/services/user.service';
 export class ICE1Component {
   neededCourses: any;
   @ViewChild('mandatoryPaginator') mandatoryPaginator!: MatPaginator;
-  @ViewChild('basicPaginator') basicPaginator!: MatPaginator;
-  @ViewChild('choiceFromSameBasicPaginator') choiceFromSameBasicPaginator!: MatPaginator;
-  @ViewChild('choiceFromOtherBasicPaginator') choiceFromOtherBasicPaginator!: MatPaginator;
+  @ViewChild('choiceFromSameBasicForSoftwarePaginator') choiceFromSameBasicForSoftwarePaginator!: MatPaginator;
+  @ViewChild('choiceFromOtherBasicForSoftwarePaginator') choiceFromOtherBasicForSoftwarePaginator!: MatPaginator;
+  @ViewChild('choiceFromSameBasicForHardwarePaginator') choiceFromSameBasicForHardwarePaginator!: MatPaginator;
+  @ViewChild('choiceFromOtherBasicForHardwarePaginator') choiceFromOtherBasicForHardwarePaginator!: MatPaginator;
+  @ViewChild('choiceFromSameBasicForNetworkPaginator') choiceFromSameBasicForNetworkPaginator!: MatPaginator;
+  @ViewChild('choiceFromOtherBasicForNetworkPaginator') choiceFromOtherBasicForNetworkPaginator!: MatPaginator;
   @ViewChild('generalPaginator') generalPaginator!: MatPaginator;
+
   mandatoryCourses!: MatTableDataSource<any>;
-  basicCourses!: MatTableDataSource<any>;
-  choiceCoursesFromSameBasic!: MatTableDataSource<any>;
-  choiceCoursesFromOtherBasic!: MatTableDataSource<any>;
+  basicCoursesForSoftware!: MatTableDataSource<any>;
+  choiceCoursesFromSameBasicForSoftware!: MatTableDataSource<any>;
+  choiceCoursesFromOtherBasicForSoftware!: MatTableDataSource<any>;
+  basicCoursesForHardware!: MatTableDataSource<any>;
+  choiceCoursesFromSameBasicForHardware!: MatTableDataSource<any>;
+  choiceCoursesFromOtherBasicForHardware!: MatTableDataSource<any>;
+  basicCoursesForNetwork!: MatTableDataSource<any>;
+  choiceCoursesFromSameBasicForNetwork!: MatTableDataSource<any>;
+  choiceCoursesFromOtherBasicForNetwork!: MatTableDataSource<any>;
   generalCourses!: MatTableDataSource<any>;
+
+  selectedOption!: string;
 
   constructor(private userService: UserService){}
 
@@ -27,17 +39,26 @@ export class ICE1Component {
     let user = this.userService.getUser();
     this.neededCourses = user.grades.neededCourses;
     this.mandatoryCourses = new MatTableDataSource(this.neededCourses.mandatoryCoursesLeft);
-    this.basicCourses = new MatTableDataSource(this.neededCourses.basicCoursesLeft);
-    this.choiceCoursesFromSameBasic = new MatTableDataSource(this.neededCourses.choiceCoursesFromSameBasicLeft);
-    this.choiceCoursesFromOtherBasic = new MatTableDataSource(this.neededCourses.choiceCoursesFromOtherBasicLeft);
+    this.basicCoursesForSoftware = new MatTableDataSource(this.neededCourses.basicCoursesLeftForSoftware);
+    this.choiceCoursesFromSameBasicForSoftware = new MatTableDataSource(this.neededCourses.choiceCoursesFromSameBasicLeftForSoftware);
+    this.choiceCoursesFromOtherBasicForSoftware = new MatTableDataSource(this.neededCourses.choiceCoursesFromOtherBasicLeftForSoftware);
+    this.basicCoursesForHardware = new MatTableDataSource(this.neededCourses.basicCoursesLeftForHardware);
+    this.choiceCoursesFromSameBasicForHardware = new MatTableDataSource(this.neededCourses.choiceCoursesFromSameBasicLeftForHardware);
+    this.choiceCoursesFromOtherBasicForHardware = new MatTableDataSource(this.neededCourses.choiceCoursesFromOtherBasicLeftForHardware);
+    this.basicCoursesForNetwork = new MatTableDataSource(this.neededCourses.basicCoursesLeftForNetwork);
+    this.choiceCoursesFromSameBasicForNetwork = new MatTableDataSource(this.neededCourses.choiceCoursesFromSameBasicLeftForNetwork);
+    this.choiceCoursesFromOtherBasicForNetwork = new MatTableDataSource(this.neededCourses.choiceCoursesFromOtherBasicLeftForNetwork);
     this.generalCourses = new MatTableDataSource(this.neededCourses.generalCoursesLeft);
   }
 
   ngAfterViewInit(){
     this.mandatoryCourses.paginator = this.mandatoryPaginator;
-    this.basicCourses.paginator = this.basicPaginator;
-    this.choiceCoursesFromSameBasic.paginator = this.choiceFromSameBasicPaginator;
-    this.choiceCoursesFromOtherBasic.paginator = this.choiceFromOtherBasicPaginator;
+    this.choiceCoursesFromSameBasicForSoftware.paginator = this.choiceFromSameBasicForSoftwarePaginator;
+    this.choiceCoursesFromOtherBasicForSoftware.paginator = this.choiceFromOtherBasicForSoftwarePaginator;
+    this.choiceCoursesFromSameBasicForHardware.paginator = this.choiceFromSameBasicForHardwarePaginator;
+    this.choiceCoursesFromOtherBasicForHardware.paginator = this.choiceFromOtherBasicForHardwarePaginator;
+    this.choiceCoursesFromSameBasicForNetwork.paginator = this.choiceFromSameBasicForNetworkPaginator;
+    this.choiceCoursesFromOtherBasicForNetwork.paginator = this.choiceFromOtherBasicForNetworkPaginator;
     this.generalCourses.paginator = this.generalPaginator;
   }
 }
