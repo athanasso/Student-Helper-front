3447b6b9aecbde900785999f73f42df41cab31a2
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2024-03-05 00:26:07 +0200

    fixed tei peir logic

--- src/app/components/services/Peir/Peir.component.ts
+++ src/app/components/services/Peir/Peir.component.ts
@@ -11,13 +11,10 @@ import { UserService } from 'src/app/services/user.service';
 export class PeirComponent {
   neededCourses: any;
   @ViewChild('mandatoryPaginator') mandatoryPaginator!: MatPaginator;
-  @ViewChild('choiceCourses1Left') choiceCourses1Paginator!: MatPaginator;
-  @ViewChild('choiceCourses2Left') choiceCourses2Paginator!: MatPaginator;
-  @ViewChild('choiceCourses3Left') choiceCourses3Paginator!: MatPaginator;
+  @ViewChild('choiceCoursesLeft') choiceCoursesPaginator!: MatPaginator;
+
   mandatoryCourses!: MatTableDataSource<any>;
-  choiceCourses1!: MatTableDataSource<any>;
-  choiceCourses2!: MatTableDataSource<any>;
-  choiceCourses3!: MatTableDataSource<any>;
+  choiceCoursesLeft!: MatTableDataSource<any>;
 
   constructor(private userService: UserService){}
 
@@ -25,15 +22,11 @@ export class PeirComponent {
     let user = this.userService.getUser();
     this.neededCourses = user.grades.neededCourses;
     this.mandatoryCourses = new MatTableDataSource(this.neededCourses.mandatoryCoursesLeft);
-    this.choiceCourses1 = new MatTableDataSource(this.neededCourses.choiceCourses1Left);
-    this.choiceCourses2 = new MatTableDataSource(this.neededCourses.choiceCourses2Left);
-    this.choiceCourses3 = new MatTableDataSource(this.neededCourses.choiceCourses3Left);
+    this.choiceCoursesLeft = new MatTableDataSource(this.neededCourses.choiceCoursesLeft);
   }
 
   ngAfterViewInit(){
     this.mandatoryCourses.paginator = this.mandatoryPaginator;
-    this.choiceCourses1.paginator = this.choiceCourses1Paginator;
-    this.choiceCourses2.paginator = this.choiceCourses2Paginator;
-    this.choiceCourses3.paginator = this.choiceCourses3Paginator;
+    this.choiceCoursesLeft.paginator = this.choiceCoursesPaginator;
   }
 }
